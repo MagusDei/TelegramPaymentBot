4c63d23502a8660aeabfe0ab8b52ada808d07b54
MagusDei <jami.valorinta@helsinki.fi>
2017-05-28 11:29:35 +0300

    Post route added

--- telepaybot/config/routes.rb
+++ telepaybot/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'create_new_bot/index'
 
+  post 'create_new_bot/index' => 'create_new_bot#create'
+  
   root 'create_new_bot#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
