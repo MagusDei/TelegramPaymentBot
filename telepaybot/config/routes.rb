944b7a9095ed822d952312846aa2d607db740c71
MagusDei <jami.valorinta@helsinki.fi>
2017-05-28 08:26:50 +0000

    Added root

--- telepaybot/config/routes.rb
+++ telepaybot/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'create_new_bot/index'
 
+  root 'create_new_bot#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
